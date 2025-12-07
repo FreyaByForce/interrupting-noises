df7e238293054c240f9714e44d750091582cb71c
FyreWolf99 <78391828+FreyaByForce@users.noreply.github.com>
2025-12-07 11:38:40 -0800

    V2
    Includes support for multiple files and documentation that doesn't suck

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Interrupting Noises
+
+A windows application for creating silence occaisionally interrupted by arbitrary WAV files.
+
+## Usage
+
+`noises.exe [filename.wav]`
+This will play **filename.wav** repeatedly between a random wait time between the default 1-10 minutes.
+
+`noises.exe [file1.wav] [file2.wav] ... --min-wait 10 --max-wait 30`
+This will randomly choose either of the files to play before waiting between 10 to 30 seconds.
+
+## Known Limitations
+
+ - This application **only works with WAV files** as that is what the `winsound` library supports.
+ - This application only works on Windows, as I have yet to find a cross-platform library for playing sound that works properly.
--- noises.py
+++ noises.py
@@ -1,30 +1,32 @@
 import sys
+import getopt
 import random
 import time
+import argparse
 import msvcrt
 from pathlib import Path
 import winsound
 
+parser = argparse.ArgumentParser(prog="Interrupting Noises", description="Creates silence occaisionally interrupted with arbitrary WAV files")
+parser.add_argument('files', nargs='+', type=str, help="Files to play")
+parser.add_argument('-s', '--min-wait', type=int, default=60, help="Minimum possible wait time between plays")
+parser.add_argument('-l', '--max-wait', type=int, default=600, help="Maximum possible wait time between plays")
+
 def main():
-    if len(sys.argv) != 2 and len(sys.argv) != 4:
-        print("Incorrect Usage!\nnoises.exe [FILENAME.wav] <minimum time> <maximum time>")
-        sys.exit()
-    
-    filename = sys.argv[1]
+    args = parser.parse_args()
 
-    random_min = 60
-    random_max = 600
+    random_min = args.min_wait
+    random_max = args.max_wait
 
-    if len(sys.argv) == 4:
-        random_min = int(sys.argv[2])
-        random_max = int(sys.argv[3])
+    filenames = args.files
     
     print("Press X to Exit!")
 
     try:
         while True:
             wait_time = random.randrange(random_min, random_max)
-            print("Playing Sound!!")
+            filename = filenames[random.randrange(0, len(filenames))]
+            print(f"Playing {filename}!")
             winsound.PlaySound(filename, winsound.SND_FILENAME)
 
             if msvcrt.kbhit():
@@ -32,8 +34,10 @@ def main():
                     sys.exit()
 
             time.sleep(wait_time)
-    except:
-        print("Error Opening File:\nMake sure you typed it in correctly and that it is a WAV!!!")
+    except SystemExit:
         sys.exit()
+    except:
+        print("Error Opening File!\nMake sure you typed it in correctly and that it is a WAV!!!")
+        sys.exit(1)
 
 main()
